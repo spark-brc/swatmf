da21047e80dfaec0e4dd92543dbdab7a8b6aed8b
Seonggyu Park <envpsg@gmail.com>
2021-03-08 13:05:44 -0600

    updated funtions

--- swatmf_pkgs/swatmf_scenario_utils.py
+++ swatmf_pkgs/swatmf_scenario_utils.py
@@ -44,6 +44,55 @@ def cvt_gcm_pcp(wd):
     print("  Done!")
 
 
+def read_pcp(pcp_path, nloc):
+    with open(pcp_path, 'r') as f:
+        content = f.readlines()
+    doy = [int(i[:7]) for i in content[4:]]
+    date = pd.to_datetime(doy, format='%Y%j')
+    df = pd.DataFrame(index=date)
+    start_num = 7
+    for i in tqdm(range(nloc)):
+        pp = [float(i[start_num:start_num+5]) for i in content[4:]]
+        start_num += 5
+        df["sub_{}".format(i+1)] = pp
+    return df
+
+
+def create_base_scn_pcp(
+        nloc, base_file, scenario_dir, base_start_date, 
+        base_end_date, scn_start_date, scn_end_date, output_dir):
+    base_df = read_pcp(base_file, nloc)
+    base_df = base_df[base_start_date:base_end_date]
+    wt_fds, full_paths = get_weather_folder_lists(scenario_dir)
+
+    for n, p in zip(wt_fds, full_paths):
+        os.chdir(p)
+        scn_df = read_pcp('pcp1.pcp', nloc)
+        scn_df = scn_df[scn_start_date:scn_end_date]
+        df = pd.concat([base_df, scn_df], axis=0)
+
+        tot_pts = len(df.columns)
+        print("  Combining base pcp and scenario pcp data in {} ... processing".format(n))
+        for i in tqdm(range(tot_pts)):
+            df.iloc[:, i] = df.iloc[:, i].map(lambda x: '{:05.1f}'.format(x))
+        df.insert(loc=0, column='year', value=df.index.year)
+        df.insert(loc=1, column='jday', value=df.index.dayofyear.map(lambda x: '{:03d}'.format(x)))
+        dff = df.T.astype(str).apply(''.join)
+
+        new_pcp_dir = os.path.join(output_dir, n)
+        if not os.path.exists(new_pcp_dir):
+            os.mkdir(new_pcp_dir)
+        try:
+            with open(os.path.join(new_pcp_dir, 'pcp1.pcp'), 'w') as f:
+                for i in range(4):
+                    f.write('\n')
+                dff.to_csv(f, line_terminator='\n', index=False, header=None)
+            print("  Combining base pcp and scenario pcp data in {} ... passed".format(n))
+        except Exception as e:
+                raise Exception("unable to copy {} from model: " + \
+                                "to new worker dir: {1}\n{2}".format(f, new_pcp_dir, str(e)))     
+
+    print("  Done!")
 
 
 def _remove_readonly(func, path, excinfo):
@@ -158,9 +207,25 @@ def extract_scenario_results(
     
 
 
+# if __name__ == '__main__':
+#     base_file = 'D:\\Projects\\Watersheds\\Okavango\\scenarios\\okvg_swatmf_scn_climates\\models\\base\\pcp1.pcp'
+#     scenario_dir = 'D:\\Projects\\Watersheds\\Okavango\\scenarios\\okvg_swatmf_scn_climates\\weather_inputs_030821'
+#     base_st = '1/1/1980'
+#     base_ed = '12/31/2019'
+#     scn_st = '1/1/2020'
+#     scn_ed = '12/31/2099'
+#     output_dir = 'D:\\Projects\\Watersheds\\Okavango\\scenarios\\okvg_swatmf_scn_climates\\combined_base_scn_pcp'
+
+
+
+#     create_base_scn_pcp(257, base_file, scenario_dir, base_st, base_ed, scn_st, scn_ed, output_dir)
+#     # print('hi')
+
+
+
 if __name__ == '__main__':
     models_wd = "D:\\Projects\\Watersheds\\Okavango\\scenarios\\okvg_swatmf_scn_climates\\models"
-    weather_wd = "D:\\Projects\\Watersheds\\Okavango\\scenarios\\okvg_swatmf_scn_climates\\weather_inputs_modified"
+    weather_wd = "D:\\Projects\\Watersheds\\Okavango\\scenarios\\okvg_swatmf_scn_climates\\combined_base_scn_pcp"
     scn_wd = "D:\\Projects\\Watersheds\\Okavango\\scenarios\\okvg_swatmf_scn_climates\\scn_models"
     mrwd = "D:\\Projects\\Watersheds\\Okavango\\scenarios\\okvg_swatmf_scn_climates\\scn_model_results_long"
     # result_files = [
@@ -178,6 +243,6 @@ if __name__ == '__main__':
     execute_scenarios(
             models_wd, weather_wd, scn_models_wd=scn_wd, reuse_models=True,
             # copy_files_fr_model=['okvg_3000.dis']
-            copy_files_fr_weather=['Tmp1.Tmp', 'pcp1.pcp']
+            copy_files_fr_weather=['pcp1.pcp']
             )
     print('hi')
