9fd9f90644eb52bb40403abf37bb549223228f1a
Seonggyu Park <envpsg@gmail.com>
2021-03-10 10:47:46 -0600

    modfied swatmf funtions

--- swatmf_pkgs/swatmf_scenario_utils.py
+++ swatmf_pkgs/swatmf_scenario_utils.py
@@ -228,21 +228,21 @@ if __name__ == '__main__':
     weather_wd = "D:\\Projects\\Watersheds\\Okavango\\scenarios\\okvg_swatmf_scn_climates\\combined_base_scn_pcp"
     scn_wd = "D:\\Projects\\Watersheds\\Okavango\\scenarios\\okvg_swatmf_scn_climates\\scn_models"
     mrwd = "D:\\Projects\\Watersheds\\Okavango\\scenarios\\okvg_swatmf_scn_climates\\scn_model_results_long"
-    # result_files = [
-    #     'output.rch',    
-    #     'output.sub',
-    #     'output.rsv',
-    #     'output.std',
-    #     'swatmf_out_MF_gwsw_monthly',
-    #     'swatmf_out_MF_head_monthly',
-    #     'swatmf_out_MF_recharge_monthly',
-    #     'swatmf_out_SWAT_gwsw_monthly',
-    #     'swatmf_out_SWAT_recharge_monthly'
-    #     ]
+    result_files = [
+        'output.rch',    
+        'output.sub',
+        'output.rsv',
+        'output.std',
+        'swatmf_out_MF_gwsw_monthly',
+        'swatmf_out_MF_head_monthly',
+        'swatmf_out_MF_recharge_monthly',
+        'swatmf_out_SWAT_gwsw_monthly',
+        'swatmf_out_SWAT_recharge_monthly'
+        ]
     # extract_scenario_results(scn_wd, result_files, model_results_wd=mrwd)
     execute_scenarios(
             models_wd, weather_wd, scn_models_wd=scn_wd, reuse_models=True,
             # copy_files_fr_model=['okvg_3000.dis']
-            copy_files_fr_weather=['pcp1.pcp']
+            # copy_files_fr_weather=['pcp1.pcp']
             )
     print('hi')
