ce41ba66fbe136f408c376156e139071ab863e13
Seonggyu Park <seonggyu.park@brc.tamus.edu>
2022-05-31 16:16:22 -0500

    updated gumu_pst_utils

--- swatmf/gumu_pst_utils.py
+++ swatmf/gumu_pst_utils.py
@@ -2,10 +2,11 @@
 """
 
 import pandas as pd
+import numpy as np
 
 def extract_stf_dates(subs, sim_start, warmup, cal_start, cal_end, dates):
     rch_file = 'output.rch'
-    start_day =  sim_start[:-4] + str(int(sim_start[-4:])+ int(warmup))
+    sim_start =  sim_start[:-4] + str(int(sim_start[-4:])+ int(warmup))
 
     rch_file = 'output.rch'
     for i in subs:
@@ -19,7 +20,7 @@ def extract_stf_dates(subs, sim_start, warmup, cal_start, cal_end, dates):
 
         sim_stf_f = sim_stf.loc[i]
         sim_stf_f = sim_stf_f.drop(['filter'], axis=1)
-        sim_stf_f.index = pd.date_range(start_day, periods=len(sim_stf_f.stf_sim))
+        sim_stf_f.index = pd.date_range(sim_start, periods=len(sim_stf_f.stf_sim))
         sim_stf_f = sim_stf_f[cal_start:cal_end]
         sim_stf_f.to_csv('stf_{:03d}.txt'.format(i), sep='\t', encoding='utf-8', index=True, header=False, float_format='%.7e')
         print('stf_{:03d}.txt file has been created...'.format(i))
@@ -28,7 +29,7 @@ def extract_stf_dates(subs, sim_start, warmup, cal_start, cal_end, dates):
 
 def extract_hg_wt_mean(hg_wt_subs, sim_start, warmup, cal_start, cal_end, dates):
     hg_rch_file = 'output-mercury.rch'
-    start_day =  start_day[:-4] + str(int(start_day[-4:])+ int(warmup))
+    sim_start =  sim_start[:-4] + str(int(sim_start[-4:])+ int(warmup))
     hg_df = pd.read_csv(hg_rch_file,
                         delim_whitespace=True,
                         skiprows=2,
@@ -57,7 +58,7 @@ def extract_hg_wt_mean(hg_wt_subs, sim_start, warmup, cal_start, cal_end, dates)
 
 def extract_hg_sed_mean(hg_sed_subs, sim_start, warmup, cal_start, cal_end, dates):
     hg_rch_file = 'output-mercury.rch'
-    start_day =  start_day[:-4] + str(int(start_day[-4:])+ int(warmup))
+    sim_start =  sim_start[:-4] + str(int(sim_start[-4:])+ int(warmup))
     hg_df = pd.read_csv(hg_rch_file,
                         delim_whitespace=True,
                         skiprows=2,
@@ -84,3 +85,67 @@ def extract_hg_sed_mean(hg_sed_subs, sim_start, warmup, cal_start, cal_end, date
                 float_format='%.7e')
         print('hg_sed_{:03d}.txt file has been created...'.format(j))
     print('Finished ...')
+
+
+def hg_obd_to_ins(hg_sim_file, obd_file, col_name, cal_start, cal_end, time_step=None):
+    """extract a simulated streamflow from the output.rch file,
+        store it in each channel file.
+
+    Args:
+        - rch_file (`str`): the path and name of the existing output file
+        - channels (`list`): channel number in a list, e.g. [9, 60]
+        - start_day ('str'): simulation start day after warmup period, e.g. '1/1/1993'
+        - end_day ('str'): simulation end day e.g. '12/31/2000'
+        - time_step (`str`): day, month, year
+
+    Example:
+        pest_utils.extract_month_stf('path', [9, 60], '1/1/1993', '12/31/2000')
+    """ 
+    if time_step is None:
+        time_step = 'day'
+    #     stfobd_file = 'stf_day.obd'
+    # if time_step == 'month':
+    #     stfobd_file = 'stf_mon.obd'
+
+
+    stf_obd = pd.read_csv(
+                        obd_file,
+                        sep='\t',
+                        usecols=['date', col_name],
+                        index_col=0,
+                        parse_dates=True,
+                        na_values=[-999, '']
+                        )
+    stf_obd = stf_obd[cal_start:cal_end]
+
+    stf_sim = pd.read_csv(
+                        hg_sim_file,
+                        delim_whitespace=True,
+                        names=["date", "stf_sim"],
+                        index_col=0,
+                        parse_dates=True)
+
+    result = pd.concat([stf_obd, stf_sim], axis=1)
+
+    result['tdate'] = pd.to_datetime(result.index)
+    result['month'] = result['tdate'].dt.month
+    result['year'] = result['tdate'].dt.year
+    result['day'] = result['tdate'].dt.day
+
+    if time_step == 'day':
+        result['ins'] = (
+                        'l1 w !{}_'.format(col_name) + result["year"].map(str) +
+                        result["month"].map('{:02d}'.format) +
+                        result["day"].map('{:02d}'.format) + '!'
+                        )
+    elif time_step == 'month':
+        result['ins'] = 'l1 w !{}_'.format(col_name) + result["year"].map(str) + result["month"].map('{:02d}'.format) + '!'
+    else:
+        print('are you performing a yearly calibration?')
+    result['{}_ins'.format(col_name)] = np.where(result[col_name].isnull(), 'l1', result['ins'])
+
+    with open(hg_sim_file+'.ins', "w", newline='') as f:
+        f.write("pif ~" + "\n")
+        result['{}_ins'.format(col_name)].to_csv(f, sep='\t', encoding='utf-8', index=False, header=False)
+    print('{}.ins file has been created...'.format(hg_sim_file))
+    return result['{}_ins'.format(col_name)]
