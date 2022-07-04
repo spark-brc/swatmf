6f33e52a2a831ef81f762d11a4aa3c7c7a39b46c
Seonggyu Park <seonggyu.park@brc.tamus.edu>
2022-07-03 22:03:05 -0500

    updated hg_handler

--- swatmf/hg/hg_handler.py
+++ swatmf/hg/hg_handler.py
@@ -37,6 +37,38 @@ class Hg(object):
         return df
 
     @property
+    def hg_sub_inter2(self):
+
+        df = pd.DataFrame(index=self.dates, columns=[c for c in range(1, self.sub_num+1)])
+
+
+
+        filename = 'swatmf_out_SWAT_rivhg'
+        # Open "swatmf_out_MF_gwsw" file
+        y = ("GW/SW", "for", "Positive:", "Negative:", "Daily", "Subbasin,")  # Remove unnecssary lines
+        with open(filename, "r") as f:
+            data = [x.strip() for x in f if x.strip() and not x.strip().startswith(y)] # Remove blank lines
+        data1 = [float(x.split()[1]) for x in data]  # make each line a list
+        df = pd.DataFrame(x, columns=['sub{:03d}'.format(i) for i in self.sub_ids])
+        df.index = pd.date_range(self.sim_start, periods=len(df))
+        return df
+
+
+    @property
+    def hg_riv_grid_inter(self):
+        filename = 'swatmf_out_RT_rivhg'
+        # Open "swatmf_out_MF_gwsw" file
+        y = ("Groundwater/Surface", "for", "Positive:", "Negative:", "Day:", "Daily")  # Remove unnecssary lines
+        with open(filename, "r") as f:
+            data = [x.strip() for x in f if x.strip() and not x.strip().startswith(y)] # Remove blank lines
+        data1 = [float(x.split()[3]) for x in data]  # make each line a list
+        x = np.reshape(data1, (len(self.dates),  268))
+        df = pd.DataFrame(x, columns=['grid_ids{:04d}'.format(i) for i in range(1, 269)])
+        df.index = pd.date_range(self.sim_start, periods=len(df))
+        return df
+
+
+    @property
     def hg_sub_contr(self):
         hg_sub_file = 'output-mercury.sub'
         hg_sub_df = pd.read_csv(hg_sub_file,
