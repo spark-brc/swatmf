c3b3eab4bbef97454b27aed3ada6c606f89c602e
Seonggyu Park <seonggyu.park@brc.tamus.edu>
2023-07-25 10:11:55 -0500

    updated modes

---
+++ models/middle_bosque_1000/SWAT/000380004.sol
@@ -0,0 +1,22 @@
+ .Sol file Watershed HRU:884 Subbasin:38 HRU:4 Luse:PAST Soil: MBRW06 Slope: 0-9999 4/9/2015 12:00:00 AM ArcSWAT 2012.10_2.15
+ Soil Name: MBRW06
+ Soil Hydrologic Group: D
+ Maximum rooting depth(m) :  710.00
+ Porosity fraction from which anions are excluded: 0.500
+ Crack volume potential of soil: 0.500
+ Texture 1                : GR-CL-GRV-CL-BR
+ Depth                [mm]:      100.00      300.00      710.00
+ Bulk Density Moist [g/cc]:        1.35        1.35        0.00
+ Ave. AW Incl. Rock Frag  :        0.23        0.16        0.02
+ Ksat. (est.)      [mm/hr]:       32.40       32.40       32.40
+ Organic Carbon [weight %]:        1.16        0.44        0.44
+ Clay           [weight %]:       27.50       27.50        0.00
+ Silt           [weight %]:       37.80       37.80        0.00
+ Sand           [weight %]:       34.70       34.70        0.00
+ Rock Fragments   [vol. %]:       29.00       45.00        0.00
+ Soil Albedo (Moist)      :        0.16        0.16        0.16
+ Erosion K                :        0.32        0.32        0.00
+ Salinity (EC, Form 5)    :        1.00        1.00        0.00
+ Soil pH                  :        8.20        8.20        0.00
+ Soil CACO3               :        0.00        0.00        0.00
+                              
